b257995b7571069aa368af6af9a922644b75c63b
darwin_quiroz <darquiroz@gmail.com>
2019-12-08 16:43:26 -0500

    Vista general dashboard

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -92,7 +92,7 @@
       <div class="row align-items-center justify-content-xl-between">
         <div class="col-xl-6">
           <div class="copyright text-center text-xl-left text-muted">
-            &copy; 2018 <a href="https://github.com/DarwinQuiroz" class="font-weight-bold ml-1" target="_blank">Darwin Quiroz</a>
+            &copy; 2019 <a href="https://github.com/DarwinQuiroz" class="font-weight-bold ml-1" target="_blank">Darwin Quiroz</a>
           </div>
         </div>
         {{-- <div class="col-xl-6">
